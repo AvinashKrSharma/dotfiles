c3b09307f67926a3968294082566330217c31406
Avinash <avinashkrsharma@live.com>
2015-10-31 12:39:27 +0530

    powerline with a better but still broken config and some useful packages

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,7 @@
+;;;package --- Summary
+;-*-Emacs-Lisp-*-
+;;; Commentary:
+
 (require 'package)
 
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
@@ -27,16 +31,13 @@ Return a list of installed packages or nil for every skipped package."
 ;; Activate installed packages
 (package-initialize)
 
-(ensure-package-installed 'evil 'magit 'powerline 'git-gutter 'web-mode 'smartparens 'helm 'org 'evil-surround 'expand-region 'smex  'company 'flycheck 'angularjs-mode 'yasnippet 'auto-complete 'projectile 'rainbow-delimiters 'smooth-scrolling 'js3-mode 'rainbow-mode)
+(ensure-package-installed 'evil 'magit 'powerline 'airline-themes 'git-gutter 'web-mode 'smartparens 'helm 'helm-projectile 'org 'evil-surround 'expand-region 'smex  'company 'flycheck 'angularjs-mode 'yasnippet 'auto-complete 'projectile 'rainbow-delimiters 'smooth-scrolling 'js3-mode 'rainbow-mode 'emmet-mode 'mmm-mode 'which-key 'sr-speedbar)
 
 (require 'evil)
 (evil-mode t)
 
 (require 'evil-surround)
 (global-evil-surround-mode 1)
-
-(require 'powerline)
-(powerline-default-theme)
 
 (require 'git-gutter)
 (global-git-gutter-mode +1)
@@ -90,6 +91,11 @@ Return a list of installed packages or nil for every skipped package."
 
 (require 'auto-complete-config)
 (ac-config-default)
+(ac-set-trigger-key "TAB")
+(ac-set-trigger-key "<tab>")
+(setq-default ac-dwim nil)
+(setq-default ac-use-menu-map t)
+(define-key ac-menu-map (kbd "<backtab>") 'ac-previous)
 
 (require 'yasnippet)
 (setq yas-snippet-dirs '("~/.emacs.d/elpa/yasnippet/snippets"))
@@ -147,8 +153,100 @@ Return a list of installed packages or nil for every skipped package."
   (setq less-css-compile-at-save t))
 (less-css-mode-setup)
 
-(when (executable-find "lessc")
+(when (executable-find "lessc"))
 
 (defun rainbow-mode-setup ()
   (add-hook 'css-mode-hook 'rainbow-mode))
 (rainbow-mode-setup)
+
+(require 'mmm-mode)
+(setq mmm-global-mode 'maybe)
+
+(which-key-mode 1)
+
+(defvar show-paren-delay 0
+  "Delay (in seconds) before matching paren is highlighted.")
+
+;; Essential settings.
+(setq inhibit-splash-screen t
+      inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(when (boundp 'scroll-bar-mode)
+  (scroll-bar-mode -1))
+(show-paren-mode 1)
+(setq visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow))
+(global-visual-line-mode nil)
+(setq-default left-fringe-width nil)
+(setq-default indent-tabs-mode nil)
+(eval-after-load "vc" '(setq vc-handled-backends nil))
+(setq vc-follow-symlinks t)
+(setq large-file-warning-threshold nil)
+(setq split-width-threshold nil)
+(setq visible-bell t)
+
+;;; File type overrides.
+(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.twig$" . web-mode))
+
+;;; Stuff for line numbers.
+(defface linum-current
+  '((t (:inherit linum :weight bold :underline "#555")))
+  "The current line number.")
+
+(defun my-linum-get-format-string ()
+  (let* ((width (max 4 (1+ (length (number-to-string
+                                    (count-lines (point-min) (point-max)))))))
+         (format (concat "%" (number-to-string width) "d "))
+         (current-line-format (concat "%-" (number-to-string width) "d ")))
+    (setq my-linum-format-string format)
+    (setq my-linum-current-line-format-string current-line-format)))
+
+(defvar my-linum-current-line-number 0)
+
+(setq linum-format 'my-linum-relative-line-numbers)
+
+(defun my-linum-relative-line-numbers (line-number)
+  (let* ((offset (abs (- line-number my-linum-current-line-number)))
+         (linum-display-value (if (= 0 offset)
+                                  my-linum-current-line-number
+                                offset))
+         (format-string (if (= my-linum-current-line-number line-number) my-linum-current-line-format-string my-linum-format-string))
+         (face (if (= my-linum-current-line-number line-number) 'linum-current 'linum)))
+    (propertize (format format-string linum-display-value) 'face face)))
+
+(defadvice linum-update (around my-linum-update)
+  (let ((my-linum-current-line-number (line-number-at-pos)))
+    ad-do-it))
+(ad-activate 'linum-update)
+
+;;; Set up relative line numbering to mimic `:set number relativenumber`.
+(global-linum-mode t)
+(add-hook 'linum-before-numbering-hook 'my-linum-get-format-string)
+
+(require 'powerline)
+(require 'airline-themes)
+(load-theme 'airline-badwolf)
+
+(require 'ido)
+    (ido-mode t)
+
+(require 'speedbar)
+(speedbar 1)
+
+;;; emacs ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a0bbe4dc3513cbd049eb95f79c467b6f19dc42979fec27a0481bb6980bd8d405" "977513781c8dd86f4f0a04dbf518df5ba496da42b71173368b305478703eea42" "e8586a76a96fd322ccb644ca0c3a1e4f4ca071ccfdb0f19bef90c4040d5d3841" "8c75217782ccea7e9f3ad2dae831487a5fb636d042263d0a0e0438d551da3224" "aab598c4d024d544b4e8b356a95ca693afa9de000b154bd2f86eed68c9e75557" "b869a1353d39ab81b19eb79de40ff3e7bb6eaad705e61f7e4dbdcb183f08c5a6" "b5fe3893c8808466711c1b55bb7e66b9c6aa2a86811783375a43e1beabb1af33" "fbcdb6b7890d0ec1708fa21ab08eb0cc16a8b7611bb6517b722eba3891dfc9dd" "532769a638787d1196bc22c885e9b85269c3fc650fdecfc45135bb618127034c" "2a5be663818e1e23fd2175cc8dac8a2015dcde6b2e07536712451b14658bbf68" "f9d34593e9dd14b2d798494609aa0fddca618145a5d4b8a1819283bc5b7a2bfd" "8e7ca85479dab486e15e0119f2948ba7ffcaa0ef161b3facb8103fb06f93b428" "beeb5ac6b65fcccfe434071d4624ff0308b5968bf2f0c01b567d212bcaf66054" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
