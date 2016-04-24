4b4ac775cd155c743d9c3b0c45dc987b21002213
AvinashKrSharma <avinashkrsharma@live.com>
2016-04-24 14:48:04 +0530

    ubuntu-install - fixes for vim python support, zshrc linking etc

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,25 +1,22 @@
 #list of packages to be installed
-packages="curl git vim tidy zsh tmux cmake python-dev default-jdk editorconfig silversearcher-ag"
+packages="git curl tidy zsh tmux cmake python-dev default-jdk editorconfig silversearcher-ag checkinstall"
 
 npm_modules="jshint js-beautify tslint node-sass less typescript-formatter csslint eslint babel-eslint eslint-plugin-react"
 
 #required beforehand(to be figured out why)
-sudo apt-get install -y build-essentials
+apt-get install -y build-essentials
 
-#upgrade before doing anything
-#sudo apt-get update
+#update before doing anything
+apt-get update
 
 #install apt-get packages
-sudo apt-get install -y $packages
+apt-get install -y $packages
 
 #for nodejs
 curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
 
-#clone dotfiles in home directory
-git clone https://github.com/AvinashKrSharma/dotfiles.git ~
-
 #install npm modules
-sudo npm install -g $npm_modules
+npm install -g $npm_modules
 
 #make required directories
 mkdir $HOME/.vim/tmp
@@ -31,7 +28,23 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 chsh -s $(which zsh)
 
 #backup default .zshrc file created by oh-my-zsh
-cp ~/.zshrc.backup ~/.zshrc
+cp ~/.zshrc ~/.zshrc.backup
+
+#build vim from source
+apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev
+apt-get remove vim vim-runtime gvim vim-tiny vim-common vim-gui-common
+git clone https://github.com/vim/vim.git ~
+./vim/configure --with-features=huge --enable-multibyte --enable-rubyinterp --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config --enable-perlinterp
+            --enable-luainterp \ --enable-gui=gtk2 --enable-cscope --prefix=/usr
+make VIMRUNTIMEDIR=/usr/share/vim/vim74
+(cd vim && make install && checkinstall)
+#phew!!
+
+#setting vim as default editor
+update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
+update-alternatives --set editor /usr/bin/vim
+update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
+update-alternatives --set vi /usr/bin/vim
 
 #create symlinks
 ln -s dotfiles/aliases ~/.aliases
