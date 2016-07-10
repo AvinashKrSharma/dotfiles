1bedaf7dddeb3dc5119bcbbaf27c1ed6909d8933
Avinash <avinashkrsharma@live.com>
2016-07-10 18:18:09 +0530

    ubuntu-install - addition of antigen

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -30,6 +30,9 @@ git clone https://github.com/rupa/z.git ~
 #backup default .zshrc file created by oh-my-zsh
 mv  ~/.zshrc ~/.zshrc.backup
 
+#antigen for zsh
+curl -L https://raw.githubusercontent.com/zsh-users/antigen/master/antigen.zsh > ~/antigen.zsh
+
 #build vim from source
 apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev
 apt-get remove vim vim-runtime gvim vim-tiny vim-common vim-gui-common
